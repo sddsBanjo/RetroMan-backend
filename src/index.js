062314c3a60df262b326dadeb28d359b3a57fb07
SDDS Banjo <168935997+sddsBanjo@users.noreply.github.com>
2026-06-25 16:18:48 -0300

    Feitas as configurações iniciais do Express e Prisma

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+
+// Configuração inicial
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5501;
+
+app.use(express.json());
+
+app.get("/test", (req, res) => {
+    res.json({ status: "OK", timestamp: new Date().toISOString() });
+});
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "🤖 RetroMan rodando!",
+        version: "1.0.0",
+        endpoints: {
+            test: "/test",
+        },
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor em http://localhost:${PORT}`);
+});
